ca73d3edc69e6caa126bdc352833406d07414364
gradikay <gradi_kayamba@yahoo.com>
2019-11-19 07:31:20 -0500

    Updates

---
+++ precache-manifest.447612a2133d53cfe9c2c5dc626916e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d08aa05c0d152d9a0e7f589cf29dba3",
+    "url": "/animal-database/index.html"
+  },
+  {
+    "revision": "08ef965fa185d297894a",
+    "url": "/animal-database/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "929a7623b20aab0c613d",
+    "url": "/animal-database/static/js/2.c2effe93.chunk.js"
+  },
+  {
+    "revision": "08ef965fa185d297894a",
+    "url": "/animal-database/static/js/main.766fb779.chunk.js"
+  },
+  {
+    "revision": "64dec77eb3fcfc62cc0a",
+    "url": "/animal-database/static/js/runtime-main.48deb4ae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/animal-database/static/media/logo.25bf045c.svg"
+  }
+]);
